9843dd5dd2d8a6a0d0b914bf93444f89c3205f69
soulayman <soulaymanjaa09@gmail.com>
2025-09-10 22:40:58 +0100

    catching errors and returning back with the error message

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -6,24 +6,34 @@ use App\Models\Education;
 use App\Models\Project;
 use App\Models\Skill;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class PortfolioController extends Controller
 {
-    
 
-    public function index(){
 
-            $user=User::with('skills','projects','experiences','educations')->find(1);
-                
-            return view('dashboard',compact('user'));
+    public function index()
+    {
+        try {
+        
+            $user = User::with('skills', 'projects', 'experiences', 'educations')->find(1);
+            return view('dashboard', compact('user'));
+        } catch (Exception $e) {
+        return back()->with(['error'=>'something went wrong']);
         }
-
-
-    public function showProject($id){
-        $project = Project::find($id);
-        return view('project',compact('project'));
+        
     }
 
+
+    public function showProject($id)
+    {
+        try {
+            $project = Project::find($id);
+            return view('project', compact('project'));
+        } catch (Exception $e) {
+            return back()->with(['error' => 'something went wrong']);
+        }
+    }
 }
